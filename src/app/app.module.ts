cfd6662ef6363077f136a8482d6da2c93f020efc
vantercarlos <ra-1882123-5@alunos.unicesumar.edu.br>
2022-02-17 21:39:47 -0400

    componente nav criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Para trabalhar com formulários no Angular 12
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+// Para realizar requisições HTTP
+import { HttpClientModule } from '@angular/common/http';
+
+// importes compartilhados
 import { CoreModule } from './core/core/core.module';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // Forms
+    FormsModule,
+    ReactiveFormsModule,
+    // Requisições http
+    HttpClientModule,
     CoreModule,
     BrowserAnimationsModule
   ],
